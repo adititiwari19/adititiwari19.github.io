f371b52ee35b5588382c81ebc77e5ed60466e399
Aditi Tiwari <aditiitiwari2000@gmail.com>
2025-02-13 17:29:08 -0600

    Added hamburger menu for mobile

--- script.js
+++ script.js
@@ -33,9 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// navigation bar in hamburger format for phones
 
-
-
+function toggleMenu() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("show");
+}
 
 
 // smooth back to top scroll
